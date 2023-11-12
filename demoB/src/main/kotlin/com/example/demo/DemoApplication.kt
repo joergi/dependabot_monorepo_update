0852e9108e280429de86b04803b7e3c7b425cc1b
Joergi <1439809+joergi@users.noreply.github.com>
2023-11-12 20:59:49 +0100

    Update DemoApplication.kt

--- demoB/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demoB/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
+	// just for testing the workflow
 	runApplication<DemoApplication>(*args)
 }
